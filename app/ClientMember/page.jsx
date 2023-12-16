8f5bc34e4a564c072c4c3cdb7c70611601f1fe82
winnyDiese <winnydiese@gmail.com>
2023-12-16 20:49:25 +0100

    Create AutProvider

--- app/ClientMember/page.jsx
+++ app/ClientMember/page.jsx
@@ -1,5 +1,16 @@
+"use client"
+
+import { useSession } from "next-auth/react"
+import { redirect } from "next/navigation"
 
 const Member = () => {
+    const {data: session } = useSession({
+        required: true,
+        onUnauthenticated(){
+            redirect("/api/auth/signin?callbackUrl=/ClientMamber")
+        }
+    })
+
     return (
         <div>
             <h1>Member server session</h1>
--- app/Member/page.jsx
+++ app/Member/page.jsx
@@ -1,4 +1,3 @@
-"use client"
 
 import { getServerSession } from "next-auth";
 import { options } from "../api/auth/[...nextauth]/options";
